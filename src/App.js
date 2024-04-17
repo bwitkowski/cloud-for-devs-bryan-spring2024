70a189a20e5129f4b10edad9a81c836c3d35da2f
Witkowski <BWitkowski@madisoncollege.edu>
2024-04-17 10:41:51 -0500

    modified with today's date

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
+    <h1>Today's Date is 04/17/2024 </h1>
     <h1>hello world from Cloud for Developers</h1>
       <ul>
         <li>hello</li>
